11644ebcff856351ee037fb27691769f259b3fef
jdaichi <94897870+jdaichi@users.noreply.github.com>
2022-01-28 13:37:47 +0900

    java2章Java初級 メソッド 課題提出

---
+++ JavaStudy/Curriculum2.java
@@ -0,0 +1,6 @@
+public class Curriculum2{
+    public static void main(String[] args) {
+        String moji = "javaはプログラム言語";
+        System.out.print(moji);
+    }
+}
---
+++ JavaStudy/chapter1/src/chapter1/Ex1.java
@@ -0,0 +1,19 @@
+package chapter1;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int[] num  = new int[3];
+			num[0] = 0;
+			num[1] = 1;
+			num[2] = 2;
+	
+		for(int i = 0; i<3; i++) {
+			 System.out.println(num[i]);	
+		}
+		
+	}
+	
+
+}
